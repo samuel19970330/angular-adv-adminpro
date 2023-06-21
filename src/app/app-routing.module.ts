b01229a372adb95c205595ebfaf602c5440a6978
Samuel Saldarriaga <samuel19970330@outlook.es>
2023-06-21 15:46:14 -0500

    Diseño y Rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+
+const routes:Routes =[
+  
+
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '**', component: NopagefoundComponent },
+  
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
